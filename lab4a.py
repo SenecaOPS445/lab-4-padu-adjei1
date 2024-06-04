e2c558d311b858a7f65f6e6a74a4e171cf3c3a32
Padu-adjei1 <padu-adjei1@myseneca.ca>
2024-06-04 14:24:04 -0400

    Added lab4a

--- lab4a.py
+++ lab4a.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 def join_sets(s1, s2):
-    # join_sets will return a set that contains every value from both s1 and s2
     return s1.union(s2)
 
 def match_sets(s1, s2):
-    # match_sets will return a set that contains all values found in both s1 and s2
-    return sorted(s1.intersection(s2))
+    return s1.intersection(s2)
 
 def diff_sets(s1, s2):
-    # diff_sets will return a set that contains all different values which are not shared between the sets
     return s1.symmetric_difference(s2)
 
 if __name__ == '__main__':
